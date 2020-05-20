ffebfcd66fdcbbf07b18cda2c8fa061f848e9f05
andydna <andydna@protonmail.ch>
2020-05-20 01:06:14 -0500

    tried to use IO#expect, don't know how to expect EOF.  improved timeout

--- lib/halfshell/terminal.rb
+++ lib/halfshell/terminal.rb
@@ -1,7 +1,10 @@
 require "open4"
+require "expect"
 
 module Halfshell
   class Terminal
+    TIMEOUT       = 60 * 60
+    WAIT_INTERVAL = 1.0/1000
 
     def Terminal.default
       Terminal.sh
@@ -20,8 +23,8 @@ module Halfshell
     def initialize(stdin:, stdout:, stderr:, pid:)
       @stdin, @stdout, @stderr, @pid = stdin, stdout, stderr, pid
 
-      @try = 0
-      @limit = 1000
+      @tries = 0
+      @max_tries = (TIMEOUT / WAIT_INTERVAL).to_i
     end
 
     def puts(what)
@@ -44,10 +47,10 @@ module Halfshell
         rescue IO::EAGAINWaitReadable
           raise_error if too_many_tries?
           if got.empty?
-            sleep(1.0/1000) #sleep(WAIT*@backoff.next)
+            sleep(WAIT_INTERVAL)
             return read_nonblock_loop(io)
           else
-            @try = 0
+            @tries = 0
             return got
           end
         end
@@ -55,7 +58,7 @@ module Halfshell
     end
 
     def too_many_tries?
-      @try >= @limit
+      @tries >= @max_tries
     end
 
     def raise_error
